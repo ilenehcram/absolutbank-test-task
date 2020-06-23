62520b988f59b75ec8210ad06813d7284c65976c
marcheneli <marcheneli@gmail.com>
2020-06-23 04:24:28 +0300

    add

---
+++ precache-manifest.439dc0c2e85bab7b813773c3e20c5a51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c03df969384227bf1bccedb8d03ca7c",
+    "url": "/absolutbank-test-task/index.html"
+  },
+  {
+    "revision": "de56a5af91393afb07bd",
+    "url": "/absolutbank-test-task/static/js/2.72be919b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/absolutbank-test-task/static/js/2.72be919b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45842add7e69b60162d4",
+    "url": "/absolutbank-test-task/static/js/main.b4ab971a.chunk.js"
+  },
+  {
+    "revision": "f4d86e1d5fba2ed6240e",
+    "url": "/absolutbank-test-task/static/js/runtime-main.4ca59b18.js"
+  }
+]);
